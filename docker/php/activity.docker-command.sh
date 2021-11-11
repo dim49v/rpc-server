f928326dd4ba45060932a89e77fc419bd6e52822
Dmitrii Vikhliaev <dim49v@yandex.ru>
2021-11-11 16:55:16 +0300

    test build

--- docker/php/activity.docker-command.sh
+++ docker/php/activity.docker-command.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+until nc -z -v -w30 ${MYSQL_HOST} ${MYSQL_PORT}
+do
+  echo "Waiting for database connection..."
+  sleep 5
+done
+
 rm -r /var/www/app/var/cache/*
 composer --working-dir=/var/www/app dump-env prod --ansi
 php /var/www/app/bin/console cache:clear
--- docker/php/dev.activity.docker-command.sh
+++ docker/php/dev.activity.docker-command.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+until nc -z -v -w30 ${MYSQL_HOST} ${MYSQL_PORT}
+do
+  echo "Waiting for database connection..."
+  sleep 5
+done
+
 symfony serve --port=80
